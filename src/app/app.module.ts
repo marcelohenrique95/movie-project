04f48dc9ee9611464691667dad97549f9cd11a4b
Marcelo Henrique <77021035+marcelohenrique95@users.noreply.github.com>
2022-05-21 21:37:20 -0300

    feat: first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { MovieService } from './core/service/movie.service';
+import { MovieComponent } from './modules/movie/movie.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MovieComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    SharedModule
+  ],
+  providers: [
+    MovieService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  url: {
+    popular_movies: 'https://api.themoviedb.org/3/discover/movie?api_key=43a66c57dc5742d89d017d146c50f869&sort_by=popularity.desc&language=pt-BR'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
